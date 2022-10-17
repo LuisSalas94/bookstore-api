ef446a0df021db755d8b064e274fcb673434dd56
Fernando Salas <luisfernandosalasg@gmail.com>
2022-10-17 15:45:39 -0500

    Enable cors

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const serverless = require("serverless-http");
 const app = express();
+app.use(cors());
 const data = require("../data.json");
 
 const router = express.Router();
